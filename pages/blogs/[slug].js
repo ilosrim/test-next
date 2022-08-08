e708684d9bc87ba8a10ee532aaeb420b2488e3e5
ilosrim <ilosrim@yahoo.com>
2022-08-08 13:32:49 -0700

    edited #goback button

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -12,7 +12,7 @@ export default function PostPage({
   }) {
   return (
     <div className='container'>
-      <Link href='/'>
+      <Link href='/blog'>
         <a className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go back</a>
       </Link>
       <div className='my-3'>
